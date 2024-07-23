3c95c3bfb645655adb2c3420914a9207ea547f94
Edgarsilbel <161778645+Edgarsilbel@users.noreply.github.com>
2024-07-22 20:49:04 -0400

    Add files via upload

---
+++ heroi.ts
@@ -0,0 +1,26 @@
+ class Heroi {
+    nome: string
+    forca: number
+    nivel : number
+        constructor(nome:string, forca:number){
+            this.nome = nome
+            this.forca = forca
+            this.nivel = 1;
+     }
+
+        olharDestemido(){
+            console.log("olhardestemido!!!")
+        }
+
+    let heroi1 = new Heroi("Artrur", 1000)
+    
+    
+    
+    let heroi2 = new Heroi("Thor", 500)
+
+    
+ 
+    
+
+ }
+  
